40e02c64d450657960774c285a537625905cbb42
chunlin <chunlin@nus.edu.sg>
2022-03-01 16:54:19 +0800

    Added Common project.

---
+++ Common/UserMessage.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Common
+{
+    public class UserMessage
+    {
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+
+        [JsonPropertyName("submittedBy")]
+        public string SubmittedBy { get; set; }
+
+        [JsonPropertyName("submittedAt")]
+        public DateTimeOffset SubmittedAt { get; set; } = DateTimeOffset.Now;
+
+        [JsonPropertyName("isDeleted")]
+        public bool IsDeleted { get; set; }
+    }
+}
